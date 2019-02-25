17b6afcb1e3d266fd9bdace79d4832de05cc14e8
li_shi_lei <shilei.li@sail-fs.com>
2019-02-25 14:21:30 +0800

    add quartz

---
+++ QuartzManage/TestJob.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using Quartz;
+
+namespace QuartzManage
+{
+    public sealed class TestJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(TestJob));
+
+
+        Task IJob.Execute(IJobExecutionContext context)
+        {
+            var task = new Task(() =>
+            {
+                _logger.InfoFormat("TestJob测试");
+            });
+            var task2 = Task.Factory.StartNew(() =>
+            {
+
+            });
+
+           return task;
+
+        }
+    }
+}
